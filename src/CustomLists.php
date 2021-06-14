32b2c7889dad6deaf90ed44ef8130cb0543ded96
Alexander Krantz <klako@hotmail.se>
2021-06-14 12:48:52 +0200

    custom list endpoint uses entity manager

--- src/CustomLists.php
+++ src/CustomLists.php
@@ -2,6 +2,9 @@
 
 namespace Scouterna\Mocknet;
 
+use Scouterna\Mocknet\Database\Model\CustomList;
+use Scouterna\Mocknet\Database\Model\CustomListRule;
+use Scouterna\Mocknet\Database\Model\Group;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 
@@ -25,105 +28,62 @@ class CustomLists extends ApiEndpoint
 
     private function getLists(Response $response): Response
     {
-        $lists = $this->db->query(
-            <<<SQL
-            SELECT
-                *
-            FROM customlists
-            WHERE `group` = {$this->db->quote($this->groupId)}
-            SQL
-        )->fetchAll();
+        /** @var Group */
+        $group = $this->entityManager->find(Group::class, $this->groupId);
+        $lists = $group->customLists;
 
-        if (empty($lists)) {
-            $response = $response->withHeader('Content-Type', 'application/json');
-            $response->getBody()->write('[]');
-            return $response;
-        }
+        $returnList = [];
 
-        $returnList = new \stdClass;
         foreach ($lists as $list) {
-            $returnList->{$list['id']} = (object) [
-                'id' => $list['id'],
-                'title' => $list['title'],
-                'description' => $list['description'],
+            $returnList[$list->id] = [
+                'id' => $list->id,
+                'title' => $list->title,
+                'description' => $list->description,
                 'list_email_key' => '',
                 'aliases' => [],
                 'owning_body_type' => 'group',
-                'owning_body_id' => intval($this->groupId)
+                'owning_body_id' => $this->groupId
             ];
-
-            $rules = $this->db->query(
-                <<<SQL
-                SELECT
-                    *
-                FROM customlistrules
-                WHERE customlist = {$this->db->quote($list['id'])}
-                SQL
-            )->fetchAll();
-
-            if (empty($rules)) {
-                $returnList->{$list['id']}->rules = [];
-                continue;
+            if ($list->rules->isEmpty()) {
+                $returnList[$list->id]['rules'] = [];
             }
-
-            $ruleList = new \stdClass;
-            foreach ($rules as $rule) {
-                $ruleList->{$rule['id']} = (object) [
-                    'id' => $rule['id'],
-                    'title' => $rule['title'],
+            $ruleList = [];
+            foreach ($list->rules as $rule) {
+                $ruleList[$rule->id] = [
+                    'id' => $rule->id,
+                    'title' => $rule->title,
                     'link' => ''
                 ];
             }
-            $returnList->{$list['id']}->rules = $ruleList;
+            $returnList[$list->id]->rules = $ruleList;
         }
         $response = $response->withHeader('Content-Type', 'application/json');
         $response->getBody()->write(\json_encode($returnList));
-
         return $response;
     }
 
     private function getListMembers($listId, Response $response): Response
     {
-        $members = $this->db->query(
-            <<<SQL
-            SELECT
-                m.member_no,
-                m.email,
-                m.first_name,
-                m.last_name
-            FROM members m
-            LEFT JOIN customlistrulemembers clrm ON clrm.member = m.member_no
-            LEFT JOIN customlistrules clr ON clr.id = clrm.customlistrule
-            WHERE clr.customlist = {$this->db->quote($listId)}
-            SQL
-        )->fetchAll();
-        $returnObject = new \stdClass;
-        if (empty($members)) {
-            $returnObject->data = [];
-            $response = $response->withHeader('Content-Type', 'application/json');
-            $response->getBody()->write(\json_encode($returnObject));
-            return $response;
+        /** @var CustomList */
+        $list = $this->entityManager->find(CustomList::class, $listId);
+        $members = [];
+        foreach ($list->rules as $rule) {
+            foreach ($rule->members as $groupMember) {
+                $member = $groupMember->member;
+                if ($members[$member->id]) {
+                    continue;
+                }
+                $members[$member->id] = [
+                    'member_no' => ['value' => $member->id],
+                    'email' => ['value' => $member->email],
+                    'extra_emails' => ['value' => []],
+                    'first_name' => ['value' => $member->first_name],
+                    'last_name' => ['value' => $member->last_name],
+                ];
+            }
         }
-        $returnObject->data = new \stdClass;
-        foreach ($members as $member) {
-            $returnObject->data->{$member['member_no']} = (object) [
-                'member_no' => (object) [
-                    'value' => $member['member_no']
-                ],
-                'email' => (object) [
-                    'value' => $member['email']
-                ],
-                'extra_emails' => (object)[
-                    'value' => []
-                ],
-                'first_name' => (object) [
-                    'value' => $member['first_name']
-                ],
-                'last_name' => (object) [
-                    'value' => $member['last_name']
-                ],
-            ];
-        }
+        $returnObject = ['data' => $members];
+
         $response = $response->withHeader('Content-Type', 'application/json');
         $response->getBody()->write(\json_encode($returnObject));
         return $response;
@@ -131,46 +91,24 @@ class CustomLists extends ApiEndpoint
 
     private function getListRuleMembers($ruleId, Response $response): Response
     {
-        $members = $this->db->query(
-            <<<SQL
-            SELECT
-                m.member_no,
-                m.email,
-                m.first_name,
-                m.last_name
-            FROM members m
-            LEFT JOIN customlistrulemembers clrm ON clrm.member = m.member_no
-            LEFT JOIN customlistrules clr ON clr.id = clrm.customlistrule
-            WHERE clr.id = {$this->db->quote($ruleId)}
-            SQL
-        )->fetchAll();
-        $returnObject = new \stdClass;
-        if (empty($members)) {
-            $returnObject->data = [];
-            $response = $response->withHeader('Content-Type', 'application/json');
-            $response->getBody()->write(\json_encode($returnObject));
-            return $response;
-        }
-        $returnObject->data = new \stdClass;
-        foreach ($members as $member) {
-            $returnObject->data->{$member['member_no']} = (object) [
-                'member_no' => (object) [
-                    'value' => $member['member_no']
-                ],
-                'email' => (object) [
-                    'value' => $member['email']
-                ],
-                'extra_emails' => (object)[
-                    'value' => []
-                ],
-                'first_name' => (object) [
-                    'value' => $member['first_name']
-                ],
-                'last_name' => (object) [
-                    'value' => $member['last_name']
-                ],
+        /** @var CustomListRule */
+        $rule = $this->entityManager->find(CustomListRule::class, $ruleId);
+        $members = [];
+        foreach ($rule->members as $groupMember) {
+            $member = $groupMember->member;
+            if ($members[$member->id]) {
+                continue;
+            }
+            $members[$member->id] = [
+                'member_no' => ['value' => $member->id],
+                'email' => ['value' => $member->email],
+                'extra_emails' => ['value' => []],
+                'first_name' => ['value' => $member->first_name],
+                'last_name' => ['value' => $member->last_name],
             ];
         }
+        $returnObject = ['data' => $members];
+        
         $response = $response->withHeader('Content-Type', 'application/json');
         $response->getBody()->write(\json_encode($returnObject));
         return $response;
