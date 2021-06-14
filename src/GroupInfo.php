b5cdd0b24b37d1a11f67fe899b82357d97023799
Alexander Krantz <klako@hotmail.se>
2021-06-14 12:05:54 +0200

    Converted group info endpoint

--- src/GroupInfo.php
+++ src/GroupInfo.php
@@ -2,6 +2,8 @@
 
 namespace Scouterna\Mocknet;
 
+use Scouterna\Mocknet\Database\Model\Group;
+use Scouterna\Mocknet\Database\Model\Member;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 
@@ -9,37 +11,39 @@ class GroupInfo extends ApiEndpoint
 {
     protected function getResponse(Request $request, Response $response, $args): Response
     {
-        $group = $this->db->query("SELECT * FROM groups WHERE id = {$this->db->quote($this->groupId)}")->fetch();
-        $membercount = $this->db->query("SELECT COUNT(*) FROM groupmembers WHERE `group` = {$this->db->quote($this->groupId)}")->fetchColumn();
-        $rolecount = $this->db->query(
-            <<<SQL
-            SELECT
-                COUNT(*)
-            FROM groupmemberroles gmr
-            LEFT JOIN groupmembers gm ON gm.id = gmr.groupmember
-            WHERE gm.`group` = {$this->db->quote($this->groupId)}
-            GROUP BY gm.id
-            SQL
-        )->fetchColumn();
-        $waitingcount = $this->db->query("SELECT COUNT(*) FROM groupwaiters WHERE `group` = {$this->db->quote($this->groupId)}")->fetchColumn();
-        $group_email = $group['group_email'] ? 'true' : 'false';
-
-        $leader = $this->db->query("SELECT first_name, last_name, email FROM members WHERE member_no = {$this->db->quote($group['leader'])}")->fetch();
-        if ($leader) {
-            $leaderInfo = <<<JSON
-            {
-                "name": "{$leader['first_name']} {$leader['last_name']}",
-                "contactdetails": "{$leader['email']}"
+        /** @var Group */
+        $group = $this->entityManager->find(Group::class, 1);
+        $membercount = $group->members->count();
+        $rolecount = (function ($group) {
+            /** @var Group $group */
+            $sum = 0;
+            foreach ($group->members as $member) {
+                if ($member->roles->count()) {
+                    $sum++;
+                }
             }
-            JSON;
-        } else {
-            $leaderInfo = <<<JSON
-            {
-                "name" : "",
-                "email": ""
+            return $sum;
+        })($group);
+        $waitingcount = $group->waiters->count();
+        $group_email = $group->groupEmail ? 'true' : 'false';
+        $leader = (function ($leader) {
+            /** @var Member $leader */
+            if ($leader) {
+                return <<<JSON
+                {
+                    "name": "{$leader->first_name} {$leader->last_name}",
+                    "contactdetails": "{$leader->email}"
+                }
+                JSON;
+            } else {
+                return <<<JSON
+                {
+                    "name": "",
+                    "contactdetails": ""
+                }
+                JSON;
             }
-            JSON;
-        }
+        })($group->leader->member);
 
         $body = <<<JSON
         {
@@ -52,8 +56,8 @@ class GroupInfo extends ApiEndpoint
                 "email": "{$group['email']}",
                 "description": "{$group['description']}"
             },
-            "Leader": $leaderInfo,
-            "projects": "Not in use."
+            "Leader": $leader,
+            "projects": "Not implemented."
         }
         JSON;
 
