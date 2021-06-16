95f585caab6e7c61eb9e1ad6e2f4826279d53599
Alexander Krantz <klako@hotmail.se>
2021-06-16 21:11:04 +0200

    Refer to doctrine dbal configuration

--- src/PhpServer.php
+++ src/PhpServer.php
@@ -13,7 +13,16 @@ class PhpServer
 
     private $process;
 
-    public function __construct(string $host, int $port, $dbParams = null)
+    /**
+     * 
+     * @param string $host 
+     * @param int $port 
+     * @param string[] $dbParams parameters for a doctrine dbal connection.
+     * See link.
+     * @link https://www.doctrine-project.org/projects/doctrine-dbal/en/2.13/reference/configuration.html#configuration
+     * @return void 
+     */
+    public function __construct(string $host, int $port, array $dbParams)
     {
         $this->host = $host;
         $this->port = $port;
