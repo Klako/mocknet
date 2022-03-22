891db6e703e2d1e74411e53971c9eac15d85208f
Alexander Krantz <klako@hotmail.se>
2022-03-22 19:40:07 +0100

    Fixed and finished test

--- tests/MemberListTest.php
+++ tests/MemberListTest.php
@@ -7,6 +7,7 @@ use Scouterna\Mocknet\Api\Members;
 use Scouterna\Mocknet\Database\ManagerFactory;
 use Slim\Psr7\Factory\ServerRequestFactory;
 use Scouterna\Mocknet\Database\Model;
+use Scouterna\Mocknet\Database\Model\Member;
 use Slim\Psr7\Response;
 
 use function PHPUnit\Framework\assertArrayHasKey;
@@ -57,14 +58,16 @@ class MemberListTest extends TestCase
     {
         $nrOfMembers = 10;
         $group = new Model\ScoutGroup();
+        $group->name = "Testgroup";
         self::$entityManager->persist($group);
-        foreach (range(0, $nrOfMembers) as $i) {
+        foreach (range(1, $nrOfMembers) as $i) {
             $member = new Model\Member();
             self::$entityManager->persist($member);
             $groupMember = new Model\GroupMember($group, $member);
             self::$entityManager->persist($groupMember);
             $group->members->add($groupMember);
         }
+        self::$entityManager->flush();
 
         $request = self::$request->withAttribute('groupId', $group->id);
         $apiResponse = self::$endpoint->__invoke($request, new Response(), []);
@@ -79,8 +82,41 @@ class MemberListTest extends TestCase
         foreach ($group->members as $groupMember) {
             $member = $groupMember->member;
             assertArrayHasKey($member->id, $data);
-            assertIsObject($data[$member->id]);
+            assertIsArray($data[$member->id]);
             $memberObj = $data[$member->id];
+            self::checkMemberVal($member->id, $memberObj, 'member_no');
+            self::checkMemberVal($member->first_name, $memberObj, 'first_name');
+            self::checkMemberVal($member->last_name, $memberObj, 'last_name');
+            self::checkMemberVal($member->ssno, $memberObj, 'ssno');
+            self::checkMemberVal($member->note, $memberObj, 'note');
+            self::checkMemberVal($member->date_of_birth->format('Y-m-d'), $memberObj, 'date_of_birth');
+            self::checkMemberVal($member->created_at->format('Y-m-d'), $memberObj, 'created_at');
+            assertEquals(Member::STATUS_ARRAY[$member->status], $memberObj['status']);
+            assertEquals(Member::SEX_ARRAY[$member->sex], $memberObj['sex']);
+            self::checkMemberVal($member->address_1, $memberObj, 'address_1');
+            self::checkMemberVal($member->postcode, $memberObj, 'postcode');
+            self::checkMemberVal($member->town, $memberObj, 'town');
+            self::checkMemberVal($member->country, $memberObj, 'country');
+            self::checkMemberVal($member->email, $memberObj, 'email');
+            self::checkMemberVal($member->contact_alt_email, $memberObj, 'contact_alt_email');
+            self::checkMemberVal($member->contact_mobile_phone, $memberObj, 'contact_mobile_phone');
+            self::checkMemberVal($member->contact_home_phone, $memberObj, 'contact_home_phone');
+            self::checkMemberVal($member->contact_mothers_name, $memberObj, 'contact_mothers_name');
+            self::checkMemberVal($member->contact_email_mum, $memberObj, 'contact_email_mum');
+            self::checkMemberVal($member->contact_mobile_mum, $memberObj, 'contact_mobile_mum');
+            self::checkMemberVal($member->contact_telephone_mum, $memberObj, 'contact_telephone_mum');
+            self::checkMemberVal($member->contact_fathers_name, $memberObj, 'contact_fathers_name');
+            self::checkMemberVal($member->contact_email_dad, $memberObj, 'contact_email_dad');
+            self::checkMemberVal($member->contact_mobile_dad, $memberObj, 'contact_mobile_dad');
+            self::checkMemberVal($member->contact_telephone_dad, $memberObj, 'contact_telephone_dad');
+            self::checkMemberVal($member->contact_leader_interest, $memberObj, 'contact_leader_interest');
         }
+    }
+
+    private static function checkMemberVal($val, $memberObj, $fieldName) {
+        if ($val === null) {
+            return;
+        }
+        assertEquals($val, $memberObj[$fieldName]['value']);
     }
 }
